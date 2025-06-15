49f3f6b45e5f64635d060bbb30dd7b747af13a0f
El_Ahmed <104298517+tavgar@users.noreply.github.com>
2025-06-15 14:43:19 +0200

    Fix accents in banner

--- internal/output/banner.go
+++ internal/output/banner.go
@@ -7,34 +7,25 @@ import (
 
 func Banner(version string) string {
 	red := "\x1b[31m"
-	white := "\x1b[37m"
-	green := "\x1b[32m"
 	yellow := "\x1b[33m"
+	green := "\x1b[32m"
 	reset := "\x1b[0m"
 
 	artLines := []string{
-		"       _______ __  ____",
-		"      / / ___//  |/  (_)___  ___  _____",
-		" __  / /\\__ \\/ /|_/ / / __ \\/ _ \\/ ___/",
-		"/ /_/ /___/ / /  / / / / / /  __/ /",
-		"\\____//____/_/  /_/_/_/ /_/\\___/_/",
+		"       _______           _",
+		"      / / ___/____ ___  (_)___  ___  _____",
+		" __  / /\\__ \\/ __ `__ \\/ / __ \\/ _ \\/ ___/",
+		"/ /_/ /___/ / / / / / / / / / /  __/ /",
+		"\\____//____/_/ /_/ /_/_/_/ /_/\\___/_/",
 	}
 
 	coloredArt := make([]string, len(artLines))
 	for i, line := range artLines {
-		var color string
-		switch {
-		case i < 2:
-			color = red
-		case i == 2:
-			color = white
-		default:
-			color = green
-		}
-		coloredArt[i] = fmt.Sprintf("%s%s%s", color, line, reset)
+		coloredArt[i] = fmt.Sprintf("%s%s%s", green, line, reset)
 	}
 
-	coloredSlogan := fmt.Sprintf("%sBij\u00ee%s %s\u2605%s %sKurdistan%s", red, reset, yellow, reset, green, reset)
 	art := strings.Join(coloredArt, "\n")
-	return fmt.Sprintf("%s\nby Tevger Xan\u00ea (Tavgar El Ahmed)\n%s\nversion %s\n", art, coloredSlogan, version)
+	versionLine := fmt.Sprintf("%sv%s%s", green, version, reset)
+	coloredSlogan := fmt.Sprintf("%sBij\u00ee%s %s\u2605%s %sKurdistan%s", red, reset, yellow, reset, green, reset)
+	return fmt.Sprintf("%s\n%s\nby Tevger Xan\u00ea (Tavgar El Ahmed)\n%s\n", art, versionLine, coloredSlogan)
 }
