a42ab094ae643b70f7b1d06cfa524d3a8a82b4fe
El_Ahmed <104298517+tavgar@users.noreply.github.com>
2025-06-15 14:27:23 +0200

    Colorize banner

--- internal/output/banner.go
+++ internal/output/banner.go
@@ -1,17 +1,40 @@
 package output
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func Banner(version string) string {
 	red := "\x1b[31m"
+	white := "\x1b[37m"
 	green := "\x1b[32m"
 	yellow := "\x1b[33m"
 	reset := "\x1b[0m"
-	art := `       _______ __  ____
-      / / ___//  |/  (_)___  ___  _____
- __  / /\__ \/ /|_/ / / __ \/ _ \/ ___/
-/ /_/ /___/ / /  / / / / / /  __/ /
-\____//____/_/  /_/_/_/ /_/\___/_/`
+
+	artLines := []string{
+		"       _______ __  ____",
+		"      / / ___//  |/  (_)___  ___  _____",
+		" __  / /\\__ \\/ /|_/ / / __ \\/ _ \\/ ___/",
+		"/ /_/ /___/ / /  / / / / / /  __/ /",
+		"\\____//____/_/  /_/_/_/ /_/\\___/_/",
+	}
+
+	coloredArt := make([]string, len(artLines))
+	for i, line := range artLines {
+		var color string
+		switch {
+		case i < 2:
+			color = red
+		case i == 2:
+			color = white
+		default:
+			color = green
+		}
+		coloredArt[i] = fmt.Sprintf("%s%s%s", color, line, reset)
+	}
+
 	coloredSlogan := fmt.Sprintf("%sBij\u00ee%s %s\u2605%s %sKurdistan%s", red, reset, yellow, reset, green, reset)
-	return fmt.Sprintf("%s\n\nby Tevger Xan\u00ea (Tavgar El Ahmed)\n%s\nversion %s\n", art, coloredSlogan, version)
+	art := strings.Join(coloredArt, "\n")
+	return fmt.Sprintf("%s\nby Tevger Xan\u00ea (Tavgar El Ahmed)\n%s\nversion %s\n", art, coloredSlogan, version)
 }
